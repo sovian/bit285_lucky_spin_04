e351e027ad5f1d89eb952bef2e25fa38e4e673ef
Dr. Brian Bansenauer <brianb@cascadia.edu>
2022-01-16 21:44:17 -0800

    Initial commit

---
+++ LuckySpin/Controllers/SpinnerController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using LuckySpin.Models;
+
+namespace LuckySpin.Controllers
+{
+    public class SpinnerController : Controller
+    {
+        /***
+         * Index Page Actions - GET and POST
+         *   GET - diplays the Player form
+         *   POST - gathers the Player info collected by the form and Redirect to Spin
+         **/
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+                return View();
+        }
+
+        //TODO: replace the int with a Player parameter and pass it onto the Spin Action
+        [HttpPost]
+        public IActionResult Index(int luck)
+        {
+            return RedirectToAction("Spin", luck);
+        }
+
+        
+        //TODO: display the Spin View passing a new Spin that includes the Player object
+        [HttpGet]
+        public IActionResult Spin(int luck)
+        {
+            return View(new Spin {  });
+        }
+    }
+}
+
---
+++ LuckySpin/Models/Spin.cs
@@ -0,0 +1,52 @@
+﻿using System;
+namespace LuckySpin.Models
+{
+    public class Spin
+    {
+        Random random = new Random();
+        private int a, b, c; //instance variables
+
+        //public Player Player { get; set; }
+
+        public int A
+        {
+            get
+            {
+                a = random.Next(10);
+                return a;
+            }
+        }
+        public int B
+        {
+            get
+            {
+                b = random.Next(10);
+                return b;
+            }
+        }
+        public int C
+        {
+            get
+            {
+                c = random.Next(10);
+                return c;
+            }
+        }
+        //TODO: Create a public getter that makes the decision to display the image or not by returning "block" or "none" for the Razor templates CSS
+        public string Display
+        {
+            get
+            {
+                if (a == 7 || b == 7 || c == 7)
+                {
+                    return "block";
+                }
+                else
+                {
+                    return "none";
+                }
+            }
+        }
+    }
+
+}
