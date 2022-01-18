13488edff9562cd33f8ddadc61d1361120e24bc9
Sovian <aouldah@gmail.com>
2022-01-18 14:54:19 -0800

    Added Player model

---
+++ LuckySpin/Models/Player.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LuckySpin.Models
+{
+    public class Player
+    {
+        public string FirstName { get; set; }
+        public int LuckNumber { get; set; }
+    }
+}
